c4d0a1167062de1d955f49ba97bbdcd324c04dd4
Mye0n9 <rlaaudtjq000201@gmail.com>
2025-04-02 11:34:43 +0900

    0402

---
+++ Python/1965.py
@@ -0,0 +1,9 @@
+n = int(input())
+board = list(map(int,input().split()))
+dp = [1]*n
+
+for i in range(n):
+    for j in range(i):
+        if board[i] > board[j]:
+            dp[i] = max(dp[i],dp[j]+1)
+print(max(dp))
